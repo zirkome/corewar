05c14bc85a23bb418037f25badc08e8cb2ec0f54
Nicolas Bridoux <nicolas.bridoux@epitech.eu>
2013-01-21 18:43:03 +0100

    Ajout de la macro READ

--- asm/main.c
+++ asm/main.c
@@ -5,13 +5,12 @@
 ** Login   <bridou_n@epitech.net>
 ** 
 ** Started on  Mon Jan 21 18:27:50 2013 nicolas bridoux
-** Last update Mon Jan 21 18:28:48 2013 nicolas bridoux
+** Last update Mon Jan 21 18:42:04 2013 nicolas bridoux
 */
 
 int	main(int ac, char *av[])
 {
 
-
-
+  my_putchar('\n');
   return (0);
 }
