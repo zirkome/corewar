271233424a67b02bd8ce8f828b3c79d3badceb2b
Rémi Robert <robert_r@epitech.eu>
2013-01-22 13:43:23 +0100

    command finalisé

--- lib/include/vm.h
+++ lib/include/vm.h
@@ -5,7 +5,7 @@
 ** Login   <bridou_n@epitech.net>
 **
 ** Started on  Wed Jan 16 17:58:40 2013 nicolas bridoux
-** Last update Tue Jan 22 13:19:32 2013 remi robert
+** Last update Tue Jan 22 13:34:49 2013 remi robert
 */
 
 #ifndef VM_H_
@@ -39,5 +39,10 @@ char	*extract_name(char *tab, int nb_carac);
 ** print_file.c
 */
 int	print_file(char *tab, int nb_carac);
+
+/*
+** check_command.c
+*/
+int	detect_fonction(int number);
 
 #endif /* LIB_H_ */
