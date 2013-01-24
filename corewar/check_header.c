3f5247d17076c76e0e72bad016266b1ad5144a1b
Guillaume Fillon <contact.gfillon@gmail.com>
2013-01-24 23:25:41 +0100

    New check_header.c

--- corewar/check_header.c
+++ corewar/check_header.c
@@ -5,14 +5,27 @@
 ** Login   <fillon_g@epitech.net>
 **
 ** Started on  Thu Jan 24 21:46:54 2013 guillaume fillon
-** Last update Thu Jan 24 22:17:12 2013 guillaume fillon
+** Last update Thu Jan 24 23:22:40 2013 guillaume fillon
 */
 
 #include "lib.h"
 #include "op.h"
 #include "vm.h"
 
-header_t		check_header(const int fd, header_t *header)
+header_t	*check_header(const int fd, header_t *header)
 {
+  int		rt;
+  int		i;
+  char		word;
+  char		buf[HEADER_LENGTH];
 
+  i = 0;
+  while ((rt = read(fd, &word, 1)) != 0 && i < HEADER_LENGTH)
+    {
+      if (rt == -1)
+	my_error("Error: Can't read the file.", 1);
+      buf[i] = word;
+      printf("%X", buf[i++]);
+    }
+  return (header);
 }
