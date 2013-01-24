6504fee99b01106bddcc1033c5076ca5e9c71ae3
Guillaume Fillon <contact.gfillon@gmail.com>
2013-01-24 23:25:00 +0100

    New check_header.c

---
+++ corewar/check_header.c
@@ -0,0 +1,18 @@
+/*
+** check_header.c for Corewar in /home/fillon_g/projets/groups/corewar/corewar
+**
+** Made by guillaume fillon
+** Login   <fillon_g@epitech.net>
+**
+** Started on  Thu Jan 24 21:46:54 2013 guillaume fillon
+** Last update Thu Jan 24 22:17:12 2013 guillaume fillon
+*/
+
+#include "lib.h"
+#include "op.h"
+#include "vm.h"
+
+header_t		check_header(const int fd, header_t *header)
+{
+
+}
