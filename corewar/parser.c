76d808988c5569888b25bf1215723026e2ab6223
Guillaume Fillon <contact.gfillon@gmail.com>
2013-01-26 13:57:37 +0100

    parser removed

--- corewar/parser.c
+++ corewar/parser.c
@@ -5,7 +5,7 @@
 ** Login   <robert_r@epitech.net>
 **
 ** Started on  Sat Jan 26 10:54:25 2013 remi robert
-** Last update Sat Jan 26 11:31:54 2013 remi robert
+** Last update Sat Jan 26 13:57:04 2013 guillaume fillon
 */
 
 #include "lib.h"
@@ -14,25 +14,4 @@
 
 void	parser_fct(t_vm *vm)
 {
-  int	interval;
-  int	i;
-
-  i = 0;
-  printf("\nParsing instruction ...\n");
-  while (i < MEM_SIZE)
-    {
-      if ((vm->mem[i] & 0xFF) - 1 <= 15 && (vm->mem[i] & 0xFF) - 1 >= 0)
-	{
-	  interval = op_tab[(vm->mem[i] & 0xFF) - 1].nbr_args + i;
-	  printf("%s ", op_tab[(vm->mem[i] & 0xFF) - 1].mnemonique);
-	  while (i < interval)
-	    {
-	      printf("%x ", vm->mem[i]);
-	      i = i + 1;
-	    }
-	  printf(" | ");
-	}
-      i = i + 1;
-    }
-  printf("\nParsing instruction fini\n");
 }
