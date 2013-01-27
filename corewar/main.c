75037ce4a2bef40816154efab55beffa899a9ee9
Rémi Robert <robert_r@epitech.eu>
2013-01-27 21:36:36 +0100

    parseur ok

--- corewar/main.c
+++ corewar/main.c
@@ -5,43 +5,11 @@
 ** Login   <robert_r@epitech.net>
 **
 ** Started on  Mon Jan 21 18:27:28 2013 remi robert
-** Last update Sun Jan 27 09:17:38 2013 remi robert
+** Last update Sun Jan 27 01:16:16 2013 guillaume fillon
 */
 
 #include "lib.h"
 #include "vm.h"
-#include "op.h"
-
-void	init_vm(t_vm **vm)
-{
-  int	i;
-
-  i = 0;
-  if ((((*vm) = malloc(sizeof(t_vm))) == NULL) ||
-      ((*vm)->mem = malloc(sizeof(mem_t) * MEM_SIZE)) == NULL)
-    my_error("Error malloc\n", 1);
-  while (i++ < MEM_SIZE)
-    (*vm)->mem[i - 1] = 0;
-  (*vm)->size_rempl = 0;
-}
-
-void	fill_mem(t_vm **vm, t_champion *champion)
-{
-  int	i;
-
-  if ((*vm)->mem[0] == 0)
-    i = 0;
-  if ((*vm)->mem[MEM_SIZE / 4] == 0)
-    i = MEM_SIZE / 4;
-  if ((*vm)->mem[MEM_SIZE / 2] == 0)
-    i = MEM_SIZE / 2;
-  if (MEM_SIZE - (*vm)->size_rempl < champion->nb_cmd)
-    my_error("Error memory\n", 1);
-  while (i++ < champion->nb_cmd)
-    (*vm)->mem[i - 1] =
-      champion->file[i -1 + (champion->nb_carac - champion->nb_cmd)] & 0xFF;
-  (*vm)->size_rempl = i;
-}
 
 void		display_usage()
 {
@@ -54,8 +22,6 @@ int		main(int argc, char **argv)
   t_proc	*l_proc;
   header_t	*header;
   int		i;
-  t_champion	*champion1;
-  t_vm		*vm;
 
   if (argc == 1 || argc > 5)
     display_usage();
@@ -75,14 +41,6 @@ int		main(int argc, char **argv)
 #endif
       i = i + 1;
     }
-  init_vm(argc - 1, argv, header);
-  if ((champion1 = malloc(sizeof(t_champion))) == NULL)
-    return (0);
-  open_file_champion(argv[1], champion1);
-  init_vm(&vm);
-  rempl_mem(&vm, champion1);
-  print_file(champion1->file, champion1);
-  print_memory(vm);
   init_vm(l_proc, header, argv, argc - 1);
   return (0);
 }
--- corewar/parser.c
+++ corewar/parser.c
@@ -5,7 +5,7 @@
 ** Login   <robert_r@epitech.net>
 **
 ** Started on  Sat Jan 26 10:54:25 2013 remi robert
-** Last update Sun Jan 27 00:35:48 2013 guillaume fillon
+** Last update Sun Jan 27 21:11:42 2013 remi robert
 */
 
 #include "lib.h"
@@ -14,5 +14,50 @@
 
 void	parser(t_vm *vm)
 {
+  int	i;
+  int	interval;
+  char	tab[4];
+  char	size[3];
+  int	indice;
 
+  i = 0;
+  size[0] = 1;
+  size[1] = 4;
+  size[2] = 2;
+  while (i < MEM_SIZE)
+    {
+      if ((vm->mem[i] & 0xFF) != 0 && (vm->mem[i] & 0xFF) < 16)
+	{
+	  interval = 0;
+	  printf("%d\n", i);
+	  printf("{%s => %X} ", op_tab[(vm->mem[i] & 0xFF) - 1].mnemonique, (vm->mem[i] & 0xFF));
+	  printf ("%d  =>> ", op_tab[(vm->mem[i] & 0xFF) - 1].nbr_args);
+	  printf("octet de codage : %X ", (vm->mem[i + 1] & 0xFF));
+	  printf(" [1 = %d]", ((vm->mem[i + 1]) >> 6) & 0x3);
+	  printf(" [2 = %d]", ((vm->mem[i + 1]) >> 4) & 0x3);
+	  printf(" [3 = %d]", ((vm->mem[i + 1]) >> 2) & 0x3);
+	  printf(" [4 = %d]", ((vm->mem[i + 1]) & 0x3));
+	  tab[0] = ((vm->mem[i + 1]) >> 6) & 0x3;
+	  tab[1] = ((vm->mem[i + 1]) >> 4) & 0x3;
+	  tab[2] = ((vm->mem[i + 1]) >> 2) & 0x3;
+	  tab[3] = ((vm->mem[i + 1])) & 0x3;
+	  indice = 0;
+	  if (((vm->mem[i] & 0xFF) - 1) == 10)
+	    size[1] = 2;
+	  while (indice < 4)
+	    {
+	      if (tab[indice] != 0)
+		interval = interval + size[(int)tab[indice] - 1];
+	      indice = indice + 1;
+	    }
+	  if ((vm->mem[i] & 0xFF) == 0x01)
+	    interval = 3;
+	  size[1] = 4;
+	  printf("interval {[%d]} ", interval);
+	  printf ("%X \n", (vm->mem[i] & 0xFF));
+	  i = i + interval + 1;
+	}
+      i = i + 1;
+    }
+  printf("\n");
 }
